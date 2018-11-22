4cbb21b8dd86d9ea839cdbd043c8daedb0d51aff
fotcorn <fotcorn@gmail.com>
2018-11-22 13:55:52 +0100

    activate django logging

--- wol/settings.py
+++ wol/settings.py
@@ -109,3 +109,20 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 
 LOGIN_URL = '/admin/login/'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
